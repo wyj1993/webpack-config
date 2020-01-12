7293ce0b5120714f9cc3b85e3fc041a8b6d1e97e
wyj <1959617212@qq.com>
2020-01-12 21:58:05 +0800

    '33'

--- b.js
+++ b.js
@@ -6,5 +6,6 @@ function counter() {
    div.onclick = function() {
       div.innerHTML = parseInt(div.innerHTML, 10) + 1; 
     };
+	console.log('小亮')
 document.body.appendChild(div); }
 export default counter;
